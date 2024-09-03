815cadf7b09732c59c41d769eda0dae3c1822b7f
Kirby Johnston <johnstk91@gmail.com>
2024-09-03 18:43:45 -0400

    content and layout updates

--- src/content/sections/work.md
+++ src/content/sections/work.md
@@ -1,3 +1,3 @@
 ## Work
 
-[Get my resume here](/resume.pdf) or via [LinkedIn](https://www.linkedin.com/in/kirby-johnston-04a743293/), or visit my [GitHub](https://github.com/spitemonster) to see practical examples of my work. See below for links to previous employers and please [contact me](#contact) if you have any questions or would like any more information.
+[Get my resume here](/Resume_Kirby_Johnston.pdf) or via [LinkedIn](https://www.linkedin.com/in/kirby-johnston-04a743293/), or visit my [GitHub](https://github.com/spitemonster) to see practical examples of my work. See below for links to previous employers and please [contact me](#contact) if you have any questions or would like any more information.
--- src/script/global.js
+++ src/script/global.js
@@ -1,16 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('a:not(.work-card):not(nav a)')
 
+    // add external link icon to all external links and open them in a new tab
     links.forEach((l) => {
         const linkUrl = new URL(l.href)
         const currentUrl = new URL(window.location.href)
 
-        // add external link icon to all external links
         if (
             linkUrl.hostname != '' &&
             linkUrl.hostname != 'localhost' &&
             linkUrl.hostname != currentUrl.hostname &&
-            !l.parentElement.classList.contains('controls')
+            !l.parentElement.classList.contains('control-bar')
         ) {
             l.setAttribute('target', '_blank')
             l.innerHTML += `<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="var(--color-accent)" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"/></svg></span>`
@@ -18,9 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 
     const navEls = document.querySelectorAll('nav li')
-    const visMode = document.querySelector('#vis-mode')
+    const visMode = document.querySelector('.icon--vis')
 
-    // setup intersection observer
+    // setup intersection observer to highlight current page in navigation
     let observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((e) => {
@@ -38,18 +38,15 @@ window.addEventListener('DOMContentLoaded', () => {
         observer.observe(f)
     })
 
+    // set base width property for each nav element; used for animations
     navEls.forEach((l) => {
         const w = `${l.offsetWidth}px`
         l.style.setProperty('--base-width', w)
     })
 
+    // toggle light/dark mode
     visMode.addEventListener('click', () => {
-        if (document.body.classList.contains('dark')) {
-            document.body.classList.remove('dark')
-            document.body.classList.add('light')
-        } else if (document.body.classList.contains('light')) {
-            document.body.classList.remove('light')
-            document.body.classList.add('dark')
-        }
+        document.body.classList.toggle('light')
+        document.body.classList.toggle('dark')
     })
 })
