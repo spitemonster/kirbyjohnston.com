40bf598c26cbd400dd569ab7ed2d0c63afb546ff
Kirby Johnston <johnstk91@gmail.com>
2024-09-20 18:55:57 -0400

    register accordion group component

---
+++ src/script/global.ts
@@ -0,0 +1,146 @@
+import './components'
+
+import ExternalIcon from '../icons/icon--external.svg?raw'
+
+// gets the direction, normalized (between 0 and 1) between two given 2d points on the page
+function getNormalizedDirection(
+    vecFrom: { X: number; Y: number },
+    vecTo: { X: number; Y: number }
+) {
+    const directionX = vecTo.X - vecFrom.X
+    const directionY = vecTo.Y - vecFrom.Y
+    const maxDistance = 500
+
+    const distance = Math.sqrt(directionX ** 2 + directionY ** 2)
+    const clampedDistance = Math.min(distance, maxDistance)
+    const normalizedDistance = clampedDistance / maxDistance
+
+    const normalizedDirection = {
+        x: (directionX / distance) * normalizedDistance,
+        y: (directionY / distance) * normalizedDistance,
+        distance: normalizedDistance,
+    }
+
+    return normalizedDirection
+}
+
+// make the computed font size easily available throughout
+window.fontSize = (): number => {
+    return Number(
+        window
+            ?.getComputedStyle(document.body)
+            ?.getPropertyValue('font-size')
+            ?.match(/\d+/)![0]
+    )
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.add('initialized')
+    // add external link icon to all external links and open them in a new tab
+    // excluding the control bar and work card links
+    const links: HTMLAnchorElement[] = Array.from(
+        document.querySelectorAll('a:not(.work-card):not(nav a)')
+    )
+
+    links.forEach((l: HTMLAnchorElement) => {
+        if (!l.href || l.href == '') {
+            return
+        }
+        const linkUrl = new URL(l.href)
+
+        const currentUrl = new URL(window.location.href)
+
+        if (
+            linkUrl.hostname != '' &&
+            linkUrl.hostname != 'localhost' &&
+            linkUrl.hostname != currentUrl.hostname
+        ) {
+            l.setAttribute('target', '_blank')
+            l.classList.add('external')
+
+            if (
+                l.closest('.control-bar') != null ||
+                l.classList.contains('img--client-logo')
+            )
+                return
+            // if not in the control bar add an external icon
+            l.innerHTML += ExternalIcon
+        }
+    })
+
+    let observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((e) => {
+                if (e.isIntersecting) {
+                    document.body.dataset.current = e.target.id
+                }
+            })
+        },
+        {
+            threshold: 0.5,
+        }
+    )
+
+    document.querySelectorAll('.frame').forEach((f) => {
+        observer.observe(f)
+    })
+
+    {
+        // set up mouse-tracking box-shadow
+        // track mouse position on move
+        const mousePosition = { X: 0, Y: 0 }
+
+        window.addEventListener('mousemove', (e) => {
+            mousePosition.X = e.clientX
+            mousePosition.Y = e.clientY
+        })
+
+        // target elements and sizes
+        const headshot: HTMLDivElement | null =
+            document.querySelector('.img--headshot')
+
+        if (!headshot) {
+            return
+        }
+
+        const headshotSize = headshot.offsetWidth
+        const headshotHalfSize = headshotSize / 2
+        const headshotCenter = {
+            X:
+                headshot.getBoundingClientRect().left +
+                window.scrollX +
+                headshotHalfSize,
+            Y:
+                headshot.getBoundingClientRect().top +
+                window.scrollY +
+                headshotHalfSize,
+        }
+
+        function tick() {
+            if (!headshot) return
+
+            headshotCenter.X =
+                headshot.getBoundingClientRect().left +
+                window.scrollX +
+                headshotHalfSize
+            headshotCenter.Y =
+                headshot.getBoundingClientRect().top +
+                window.scrollY +
+                headshotHalfSize
+
+            const delta = getNormalizedDirection(headshotCenter, mousePosition)
+
+            // this custom property is used in CSS to control the box shadow
+            headshot.style.setProperty('--delta-x', `${delta.x}`)
+            headshot.style.setProperty('--delta-y', `${delta.y}`)
+            headshot.style.setProperty('--dist', `${delta.distance}`)
+
+            requestAnimationFrame(tick)
+        }
+
+        // only tick this on desktop
+        if (window.innerWidth > window.fontSize() * 60) {
+            requestAnimationFrame(tick)
+        }
+    }
+})
