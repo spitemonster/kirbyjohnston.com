5bf77f4c6b68d2a16bcfe5f594cdd0bf8d4b7e43
Kirby Johnston <johnstk91@gmail.com>
2026-06-23 17:38:36 -0400

    refactor: rename shuffle array method

--- src/script/utils/index.ts
+++ src/script/utils/index.ts
@@ -1,2 +1,2 @@
-export { shuffleArray } from './shuffleArray'
+export { shuffleDomNodeArray as shuffleArray } from './shuffleDomNodeArray'
 export { getNormalizedDirection } from './getNormalizedDirection'
